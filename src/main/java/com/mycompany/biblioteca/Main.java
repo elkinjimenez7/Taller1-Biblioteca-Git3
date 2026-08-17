223e71749b1b29731eab62c801082d0098dc1e94
elkinjimenez7 <edanieljimenez@unicesar.edu.co>
2026-08-16 22:01:48 -0500

    feat: implementar Create de cliente

--- src/main/java/com/mycompany/biblioteca/Main.java
+++ src/main/java/com/mycompany/biblioteca/Main.java
@@ -4,11 +4,22 @@
 
 package com.mycompany.biblioteca;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author elkin jimenez
  */
 public class Main {
+    
+    static ArrayList<Customer> customer = new ArrayList<>();
+    static Scanner sc = new Scanner(System.in);
+    
+    public void createCustomer(String kindCustomer, int id, String name, String cc, int age){
+        
+        Customer newCustomer = new Customer(kindCustomer, id, name, cc, age);
+    }
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
--- src/main/java/com/mycompany/biblioteca/Person.java
+++ src/main/java/com/mycompany/biblioteca/Person.java
@@ -10,4 +10,40 @@ package com.mycompany.biblioteca;
  */
 public class Person {
     
+    private String name;
+    private String cc;
+    private int age;
+
+    public Person(String name, String cc, int age) {
+        this.name = name;
+        this.cc = cc;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCc() {
+        return cc;
+    }
+
+    public void setCc(String cc) {
+        this.cc = cc;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    
+    
 }
